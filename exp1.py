c3f42e671bc36b438314f2dd9b7d96e75e4d4b0c
khawkins1 <khawkins1@users.noreply.github.com>
2015-07-14 16:28:11 -0700

    Update exp1.py

--- exp1.py
+++ exp1.py
@@ -2,7 +2,7 @@ print "How old are you?",
 age = raw_input()
 print "How tall are you?",
 height = raw_input()
-print "How much do you weigh?"
+print "How much do you really weigh?"
 weight = raw_input()
 
 print "So, you're %r old, %r tall and %r heavy." % (
